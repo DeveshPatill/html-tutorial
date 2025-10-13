8df89d169885be5be90ae6292e97d124cf5557c9
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-10-13 14:08:48 +0530

    Add files via upload

---
+++ JS/guess_the_number.js
@@ -0,0 +1,17 @@
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// function guessNumber() {
+//   let u_guess = prompt("Enter a number between 1 to 100:");
+
+//   if(u_guess < randomNumber) {
+//     console.log("Too Low, Type Higher Number.");
+//     guessNumber(); 
+//   }else if (u_guess > randomNumber)  {
+//     console.log("Too High, Type Lower Number.");
+//     guessNumber(); 
+//   }else{
+//     console.log(" YOU GUESSED THE CORRECT NUMBER!");
+//   }
+// }
+// guessNumber();   
+
