656893cd5c3eb43c1b01be4bba181bdafa39adfd
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-11-11 20:45:37 +0530

    Add files via upload

---
+++ JS/promises.js
@@ -0,0 +1,18 @@
+// we create promise in javascript to perform the code.
+
+
+// const tailor = new Promise(function(silakedunga,nhidunga){
+//     silakedunga("hey")
+//     nhidunga("hi")
+// })
+
+// tailor.then(
+//     function(right){    //----> whatever if u feel u can add argument in this function
+//         console.log(right)
+//         console.log("haan silgaya, tailor ne sila ke diya")
+//     },
+//     function(eror){    //----> whatever if u feel u can add argument in this function
+//         console.log(eror)
+//         console.log("haan, nhi hua mujse")
+//     }
+// ) 
