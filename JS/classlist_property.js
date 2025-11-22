70e1eb7cb91307203a7370b6f6765a2ee93b5260
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-11-22 20:55:17 +0530

    Add files via upload

---
+++ JS/classlist_property.js
@@ -0,0 +1,11 @@
+// function addclass(){
+//     const list = document.getElementById("mypara").classList.add("box");
+// }
+
+function toggleme(){
+    const list1 = document.getElementById('tog').classList.toggle('box');
+}
+
+// function removeclass(){
+//     document.getElementById('mypara').classList.remove('hidden');
+// }
